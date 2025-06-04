bd876bbc417cdce988799e2f1ba148461fbafb2f
Felipe Reis <54824417+KingsGlaad@users.noreply.github.com>
2025-06-04 10:33:52 -0300

    Atualizar o layout.tsx
    fiz: removendo erro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={`${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Toaster />
           {children}
         </ThemeProvider>
